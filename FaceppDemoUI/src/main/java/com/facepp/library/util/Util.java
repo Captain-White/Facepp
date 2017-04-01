a7d572da1f26619550027f2b5ac150483c11b7e6
Captain-White <zhou1210@live.com>
2017-04-01 10:00:35 +0800

    demo联网失败错误

---
+++ FaceppDemoUI/src/main/java/com/facepp/library/util/Util.java
@@ -0,0 +1,9 @@
+package com.facepp.library.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "-YmbvW7pkskj4oNW9-a2O1R1aCQd_q";
+	public static String API_SECRET = "POhUC7boMmqiP4wYXN5KmtEZ9jLPACPJ";
+	
+}
